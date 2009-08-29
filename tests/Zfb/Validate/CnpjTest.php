66991fcc3635d9873edb8f0f7193c0f09ee52ac7
Diego Tremper <diegotremper@gmail.com>
2009-08-28 22:08:46 -0300

    Added tests for Validate

---
+++ tests/Zfb/Validate/CnpjTest.php
@@ -0,0 +1,80 @@
+<?php
+
+
+require_once 'Zfb/Validate/Cnpj.php';
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+/**
+ * Zfb_Validate_Cnpj test case.
+ */
+class Zfb_Validate_CnpjTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var Zfb_Validate_Cnpj
+	 */
+	private $Zfb_Validate_Cnpj;
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp() {
+		parent::setUp();
+		$this->Zfb_Validate_Cnpj = new Zfb_Validate_Cnpj(/* parameters */);
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown() {
+		$this->Zfb_Validate_Cnpj = null;
+		parent::tearDown();
+	}
+
+	/**
+	 * Tests Zfb_Validate_Cnpj->isValid()
+	 */
+	public function testIsValid() {
+		$valid = array('03.847.655/0001-98');
+
+		foreach ($valid as $cnpj) {
+			$this->assertTrue($this->Zfb_Validate_Cnpj->isValid($cnpj));
+		}
+	}
+
+	public function testInvalid() {
+		$invalid = array(
+			'03.847.655/000198', //invalid format
+			'03.847.655/0001-90'
+		);
+
+		foreach ($invalid as $cnpj) {
+			$this->assertFalse($this->Zfb_Validate_Cnpj->isValid($cnpj));
+		}
+	}
+
+	public function testValidIgnoreFormat() {
+		$v = new Zfb_Validate_Cnpj ( true ); //ignore format
+		$valid = array(
+			'03.847.655/0001-98',
+			'03847655000198'
+		);
+
+		foreach ($valid as $cnpj) {
+			$this->assertTrue($v->isValid($cnpj));
+		}
+	}
+
+	public function testInvalidIgnoreFormat() {
+		$v = new Zfb_Validate_Cnpj ( true ); //ignore format
+		$invalid = array(
+			'03847655000190'
+		);
+
+		foreach ($invalid as $cnpj) {
+			$this->assertFalse($v->isValid($cnpj));
+		}
+	}
+
+}
+
---
+++ tests/Zfb/Validate/CpfTest.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once 'Zfb/Validate/Cpf.php';
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+/**
+ * Zfb_Validate_Cpf test case.
+ */
+class Zfb_Validate_CpfTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var Zfb_Validate_Cpf
+	 */
+	private $Zfb_Validate_Cpf;
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp() {
+		parent::setUp();
+		$this->Zfb_Validate_Cpf = new Zfb_Validate_Cpf(/* parameters */);
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown() {
+		$this->Zfb_Validate_Cpf = null;
+		parent::tearDown();
+	}
+
+	/**
+	 * Tests Zfb_Validate_Cpf->isValid()
+	 */
+	public function testIsValid() {
+		$valid = array(
+			'123.456.789-09'
+		);
+
+		foreach ($valid as $cpf) {
+			$this->assertTrue($this->Zfb_Validate_Cpf->isValid($cpf));
+		}
+	}
+
+	/**
+	 * Tests Zfb_Validate_Cpf->isValid()
+	 */
+	public function testIsInvalid() {
+		$invalid = array(
+			'123.456.78909',
+			'123.456.789-08'
+		);
+
+		foreach ($invalid as $cpf) {
+			$this->assertFalse($this->Zfb_Validate_Cpf->isValid($cpf));
+		}
+	}
+
+	/**
+	 * Tests Zfb_Validate_Cpf->isValid()
+	 */
+	public function testIsValidIgnoreFormat() {
+		$v = new Zfb_Validate_Cpf(true);
+
+		$valid = array(
+			'123.456.789-09',
+			'12345678909'
+		);
+
+		foreach ($valid as $cpf) {
+			$this->assertTrue($v->isValid($cpf));
+		}
+	}
+
+	/**
+	 * Tests Zfb_Validate_Cpf->isValid()
+	 */
+	public function testIsInvalidIgnoreFormat() {
+		$v = new Zfb_Validate_Cpf(true);
+
+		$valid = array(
+			'12345678900'
+		);
+
+		foreach ($valid as $cpf) {
+			$this->assertFalse($v->isValid($cpf));
+		}
+	}
+
+}
+
